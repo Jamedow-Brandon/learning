f035eb46dab34df017ee7509ce3cdfdc769ad75a
hqc <WXDYDDWJ2008@126.com>
2016-12-19 15:28:48 +0800

    service 简化接口

--- first-spring-step/src/main/java/com/jamedow/learning/utils/tbk/TbkItemGetResponse.java
+++ first-spring-step/src/main/java/com/jamedow/learning/utils/tbk/TbkItemGetResponse.java
@@ -3,7 +3,9 @@ package com.jamedow.learning.utils.tbk;
 import com.taobao.api.TaobaoResponse;
 
 /**
- * Created by 365 on 2016/12/19 0019.
+ * Description 淘宝客商品列表返回
+ *
+ * create by Jamedow at 2016-12-19
  */
 public class TbkItemGetResponse extends TaobaoResponse {
 }
