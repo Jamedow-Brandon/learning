07d3a2800550239c9237bd6a6b0a61cb2f86352a
hqc <WXDYDDWJ2008@126.com>
2016-12-21 14:41:18 +0800

    filter bootstrap

---
+++ first-spring-step/src/main/java/com/jamedow/learning/common/system/filter/LearningFilter.java
@@ -0,0 +1,30 @@
+package com.jamedow.learning.common.system.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * Description
+ * <p>
+ * Created by 365 on 2016/12/21 0021.
+ */
+public class LearningFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+        //将控制器传向下一个filter
+        chain.doFilter(servletRequest, servletResponse);
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        request.setAttribute("contextPath", request.getContextPath());
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
