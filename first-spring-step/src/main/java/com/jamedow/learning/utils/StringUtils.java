6757dec2f30815f6c39c083f7a0d6ca827cc8a4a
Jamedow-Brandon <652972575@qq.com>
2017-02-22 13:53:44 +0800

    test

--- first-spring-step/src/main/java/com/jamedow/learning/utils/StringUtils.java
+++ first-spring-step/src/main/java/com/jamedow/learning/utils/StringUtils.java
@@ -32,12 +32,7 @@ public class StringUtils extends org.apache.commons.lang.StringUtils {
     }
 
     private static boolean isEmojiCharacter(char codePoint) {
-        return (codePoint == 0x0) ||
-                (codePoint == 0x9) ||
-                (codePoint == 0xA) ||
-                (codePoint == 0xD) ||
-                ((codePoint >= 0x20) && (codePoint <= 0xD7FF)) ||
-                ((codePoint >= 0xE000) && (codePoint <= 0xFFFD)) ||
-                ((codePoint >= 0x10000) && (codePoint <= 0x10FFFF));
+        return codePoint == 0x0 || codePoint == 0x9 || codePoint == 0xA || codePoint == 0xD
+                || codePoint >= 0x20 && codePoint <= 0xD7FF || codePoint >= 0xE000 && codePoint <= 0xFFFD;
     }
 }
