0b78c58d8e8fb99006ec4bd23e506347a4a59ed1
Jamedow-Brandon <652972575@qq.com>
2017-09-08 17:55:13 +0800

    零食

---
+++ first-spring-step/src/main/java/com/jamedow/laodoufang/entity/RecipeComment.java
@@ -0,0 +1,85 @@
+package com.jamedow.laodoufang.entity;
+
+import java.util.Date;
+
+public class RecipeComment {
+    private Integer id;
+
+    private Integer recipeId;
+
+    private Integer userId;
+
+    private Date createTime;
+
+    private String content;
+
+    private Integer voteUp;
+
+    private Integer replyId;
+
+    private Integer topicId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getRecipeId() {
+        return recipeId;
+    }
+
+    public void setRecipeId(Integer recipeId) {
+        this.recipeId = recipeId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getVoteUp() {
+        return voteUp;
+    }
+
+    public void setVoteUp(Integer voteUp) {
+        this.voteUp = voteUp;
+    }
+
+    public Integer getReplyId() {
+        return replyId;
+    }
+
+    public void setReplyId(Integer replyId) {
+        this.replyId = replyId;
+    }
+
+    public Integer getTopicId() {
+        return topicId;
+    }
+
+    public void setTopicId(Integer topicId) {
+        this.topicId = topicId;
+    }
+}
